86ba7da1ed3450de8952f4e2d700da8f2521bef9
Norief26 <norief26@hotmail.com>
2021-02-06 04:46:58 -0800

    Home: Feature Complete

--- src/components/content/style.js
+++ src/components/content/style.js
@@ -4,14 +4,14 @@ export const Container = styled.div`
     background-color: #F9FAFC;
 `
 export const Group = styled.div`
+    display: flex;
+    justify-content: center;
     min-width: 500px;
-    /* align-items: center; */
-    /* justify-items: center; */
     background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0)), url(${({ src }) => (src)}) bottom / cover;
 
-    /* > * {
+    > * {
         &:first-child {
             width: 1580px;
         }
-    } */
+    }
 `
